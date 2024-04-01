7e572f4959bb3064c9444e25919ec434c799ecd1
mattroz <matveyrozanov@gmail.com>
2024-04-01 16:32:49 +0000

    feat(src/utils/hf_helpers.py): added trainable parameters calculation function for logging purposes

--- src/utils/hf_helpers.py
+++ src/utils/hf_helpers.py
@@ -5,3 +5,17 @@ def unwrap_model(accelerator, model):
     model = accelerator.unwrap_model(model)
     model = model._orig_mod if is_compiled_module(model) else model
     return model
+
+
+def get_trainable_parameters_str(model) -> str:
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+
+    return f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"

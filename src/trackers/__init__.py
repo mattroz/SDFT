279795574e69bf1f67602f14c1a85e26fddeeaff
mattroz <matveyrozanov@gmail.com>
2024-04-03 09:48:11 +0000

    feat(src/trackers/): added FileSystemTracker

---
+++ src/trackers/__init__.py
@@ -0,0 +1,1 @@
+from .file_system_tracker import FileSystemTracker
---
+++ src/trackers/file_system_tracker.py
@@ -0,0 +1,37 @@
+import os
+import json
+
+from typing import Optional
+from accelerate.tracking import GeneralTracker, on_main_process
+
+
+class FileSystemTracker(GeneralTracker):
+    name = "file_system_tracker"
+    requires_logging_directory = True
+
+    @on_main_process
+    def __init__(self, logging_dir: Optional[str | os.PathLike] = None):
+        self.path_to_log_file = os.path.join(logging_dir, "scalars.json")
+        self.config = None
+        self.run = []
+
+    @property
+    def tracker(self):
+        return self.run
+
+    @on_main_process
+    def store_init_configuration(self, values: dict):
+        self.config = values
+
+    @on_main_process
+    def log(self, values: dict, step: Optional[int] = None):
+        log_entry = values.copy()
+        log_entry["step"] = step
+        self.run.append(log_entry)
+        self._dump_to_file(log_entry)
+        
+    @on_main_process
+    def _dump_to_file(self, entry):
+        with open(self.path_to_log_file, "a") as f:
+            json.dump(entry, f)
+            f.write("\n")

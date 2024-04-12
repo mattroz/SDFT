947c95fa5984be3eafedbd535a9b863f4021e29a
mattroz <matveyrozanov@gmail.com>
2024-04-12 05:55:06 +0000

    feat(run_lora_inference.py): move out generating and saving routine to a function; add comparison flag

--- run_lora_inference.py
+++ run_lora_inference.py
@@ -2,6 +2,7 @@
 # https://github.com/huggingface/diffusers
 
 import cv2
+import datetime
 import pathlib
 
 import datasets
@@ -17,9 +18,30 @@ from diffusers import (
     AutoencoderKL,
     StableDiffusionXLPipeline,
 )
+from diffusers.utils.torch_utils import randn_tensor
 
 from src.utils.logger import get_logger
 from src.methods.lora.arguments import parse_inference_args
+
+
+def generate_and_save(args, accelerator, pipeline, latents, path_to_save_images):
+    for i in range(args.num_images_to_generate):
+        image = pipeline(
+            latents=latents,
+            num_inference_steps=args.num_inference_steps,
+            guidance_scale=args.guidance_scale,             
+            prompt=args.prompt,
+            prompt_2=args.prompt_2,
+            negative_prompt=args.negative_prompt,
+            negative_prompt_2=args.negative_prompt_2
+        ).images[0]
+        
+        if accelerator.is_main_process:
+            ts_now = datetime.datetime.now().isoformat(timespec="seconds")
+            path_to_save_image = pathlib.Path(path_to_save_images, 
+                                            "_".join(args.prompt.lower().replace(",","").replace(".", "").split(" "))+f"_{i}_{ts_now}.png")
+            image = cv2.cvtColor(np.asarray(image), cv2.COLOR_BGR2RGB)
+            cv2.imwrite(str(path_to_save_image), image)
 
 
 def main(args):
@@ -59,7 +81,7 @@ def main(args):
         revision=args.revision,
         variant=args.variant,
     )
-
+    
     # For mixed precision training we cast all non-trainable weights (vae, non-lora text_encoder and non-lora unet) to half-precision
     # as these weights are only used for inference, keeping weights in full precision is not required.
     weight_dtype = torch.float32
@@ -81,28 +103,28 @@ def main(args):
         torch_dtype=weight_dtype,
     ).to(accelerator.device)
 
-    # load LoRA weights and inject them to cross-attention in UNet
-    pipeline.load_lora_weights(args.lora_checkpoint_path, adapter_name="dark_fantasy")
-
-    # actually do not need this here, but in case you want to merge multiple LoRAs - here you go
-    pipeline.set_adapters(["dark_fantasy"], adapter_weights=[1.0])
-    
-    # run inference
     generator = torch.Generator(device=accelerator.device).manual_seed(args.seed) if args.seed else None
-    for i in range(args.num_images_to_generate):
-        image = pipeline(
-            num_inference_steps=args.num_inference_steps,
-            guidance_scale=args.guidance_scale,             
-            prompt=args.prompt,
-            prompt_2=args.prompt_2,
-            negative_prompt=args.negative_prompt,
-            negative_prompt_2=args.negative_prompt_2,
-            generator=generator
-        ).images[0]
-        path_to_save_image = pathlib.Path(path_to_save_images, 
-                                          "_".join(args.prompt.lower().replace(",","").replace(".", "").split(" "))+f"_{i}.png")
-        image = np.asarray(image)
-        cv2.imwrite(str(path_to_save_image), image)
+
+    latents_shape = (1, 
+                     pipeline.unet.config.in_channels, 
+                     args.resolution // pipeline.vae_scale_factor, 
+                     args.resolution // pipeline.vae_scale_factor)
+    latents = randn_tensor(latents_shape, generator, device=accelerator.device, dtype=weight_dtype)
+
+    if args.comparison:
+        # run inference NO LORA
+        path_to_save_nolora_images = pathlib.Path(path_to_save_images, "no_lora")
+        path_to_save_nolora_images.mkdir(parents=True, exist_ok=True)
+        generate_and_save(args, accelerator, pipeline, latents, path_to_save_nolora_images)
+
+    if args.lora_checkpoint_path is not None:
+        # load LoRA weights and inject them to cross-attention in UNet
+        pipeline.load_lora_weights(args.lora_checkpoint_path, adapter_name="dark_fantasy")
+        # actually do not need this here, but in case you want to merge multiple LoRAs - here you go
+        pipeline.set_adapters(["dark_fantasy"], adapter_weights=[1.0])
+    
+    # run inference LORA
+    generate_and_save(args, accelerator, pipeline, latents, path_to_save_images)
         
 
 if __name__ == "__main__":

b0ef9a3870c14c6dd606dc4da1e66649bf9d0f50
mattroz <matveyrozanov@gmail.com>
2024-04-20 19:51:45 +0000

    fix(src/data/dataset.py): fix output dict key name; uncommented caption templates

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -188,35 +188,35 @@ PIL_INTERPOLATION = {
     "nearest": PIL.Image.Resampling.NEAREST,
 }
 
-
+# Textual inversion paper, section D
 imagenet_templates_small = [
     "a photo of a {}",
     "a rendering of a {}",
     "a cropped photo of the {}",
     "the photo of a {}",
-    # "a photo of a clean {}",
-    # "a photo of a dirty {}",
+    "a photo of a clean {}",
+    "a photo of a dirty {}",
     "a dark photo of the {}",
     "a photo of my {}",
-    # "a photo of the cool {}",
-    # "a close-up photo of a {}",
+    "a photo of the cool {}",
+    "a close-up photo of a {}",
     "a bright photo of the {}",
     "a cropped photo of a {}",
     "a photo of the {}",
     "a good photo of the {}",
     "a photo of one {}",
-    # "a close-up photo of the {}",
+    "a close-up photo of the {}",
     "a rendition of the {}",
     "a photo of the clean {}",
     "a rendition of a {}",
     "a photo of a nice {}",
     "a good photo of a {}",
     "a photo of the nice {}",
-    # "a photo of the small {}",
-    # "a photo of the weird {}",
-    # "a photo of the large {}",
-    # "a photo of a cool {}",
-    # "a photo of a small {}",
+    "a photo of the small {}",
+    "a photo of the weird {}",
+    "a photo of the large {}",
+    "a photo of a cool {}",
+    "a photo of a small {}",
 ]
 
 
@@ -321,7 +321,7 @@ class TextualInversionDataset(Dataset):
 
         example["crop_top_left"] = (y1, x1)
 
-        example["input_ids_one"] = self.tokenizer_one(
+        example["input_id_one"] = self.tokenizer_one(
             text,
             padding="max_length",
             truncation=True,
@@ -329,7 +329,7 @@ class TextualInversionDataset(Dataset):
             return_tensors="pt",
         ).input_ids[0]
 
-        example["input_ids_two"] = self.tokenizer_two(
+        example["input_id_two"] = self.tokenizer_two(
             text,
             padding="max_length",
             truncation=True,
